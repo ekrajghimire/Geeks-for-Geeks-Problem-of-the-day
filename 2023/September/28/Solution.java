21247b0d1a2b06cbeadcbdcdfa73c6d8bb988e51
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-09-30 00:12:55 +0530

    Create Solution.java

---
+++ 2023/September/28/Solution.java
@@ -0,0 +1,12 @@
+//  Wave Array
+
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        // code here
+        for(int i=0;i<n-1;i+=2){
+            int tmp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=tmp;
+        }
+    }
+}
