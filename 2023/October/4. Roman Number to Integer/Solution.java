c7e653917ac75f3439c0f555b6cc9cece28d98a4
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-10-05 00:01:26 +0530

    Create Solution.java

---
+++ 2023/October/4. Roman Number to Integer/Solution.java
@@ -0,0 +1,29 @@
+// Roman Number to Integer
+// October 5, 2023
+// Java Code
+
+import java.util.HashMap;
+
+class Solution {
+    public int romanToDecimal(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int sum = map.get(s.charAt(s.length() - 1));
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum -= map.get(s.charAt(i));
+            } else {
+                sum += map.get(s.charAt(i));
+            }
+        }
+        
+        return sum;
+    }
+}
