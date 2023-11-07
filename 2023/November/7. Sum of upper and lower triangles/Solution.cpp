d1460cd1d28b3c25c0195fbcaee2b400787dacae
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-07 21:59:02 +0530

    Create Solution.cpp

---
+++ 2023/November/7. Sum of upper and lower triangles/Solution.cpp
@@ -0,0 +1,23 @@
+// Sum of upper and lower triangles
+// November 7, 2023
+// C++ Code
+
+class Solution
+{   
+    public:
+    //Function to return sum of upper and lower triangles of a matrix.
+    vector<int> sumTriangles(const vector<vector<int> >& matrix, int n)
+    {
+        vector<int> ans(2,0);
+        
+        for(int i=0; i<n; i++)
+            for(int j=i; j<n; j++)
+                ans[0]+=matrix[i][j];
+        
+        for(int i=0; i<n; i++)
+            for(int j=0; j<=i; j++)
+                ans[1]+=matrix[i][j];
+                
+        return ans;
+    }
+};
