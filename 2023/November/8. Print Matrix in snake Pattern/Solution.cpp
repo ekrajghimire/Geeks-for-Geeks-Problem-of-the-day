0bbb52e55d9bdaebbcfcbe67e56c63a240cfdbe4
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-08 12:06:43 +0530

    Create Solution.cpp

---
+++ 2023/November/8. Print Matrix in snake Pattern/Solution.cpp
@@ -0,0 +1,33 @@
+// Print Matrix in snake Pattern
+// November 8, 2023
+// C++ Code
+
+class Solution
+{   
+    public:
+    //Function to return list of integers visited in snake pattern in matrix.
+    vector<int> snakePattern(vector<vector<int> > matrix)
+    {   
+        int N=matrix.size();
+        vector<int> ans;
+        int flag=0;
+        for(int i=0;i<N;i++)
+        {
+            if(flag==0)
+            {
+                while(flag<N)
+                ans.push_back(matrix[i][flag++]);
+                flag=N-1;
+            }
+            else 
+            {
+                while(flag>=0)
+                ans.push_back(matrix[i][flag--]);
+                flag=0;
+            }
+            
+            
+        }
+        return ans;
+    }
+};
