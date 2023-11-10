5b9850bb083a8e46a716bf64f26a74a214b17268
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-10 21:48:34 +0530

    Create Solution.cpp

---
+++ 2023/November/10. Number following a pattern/Solution.cpp
@@ -0,0 +1,23 @@
+// Number following a pattern
+// November 10, 2023
+// C++ Code
+
+class Solution{   
+public:
+    string printMinNumberForPattern(string s){
+        vector<int>v;
+        int n=s.size();
+        string ans;
+        for(int i=0;i<=n;i++) v.push_back(i+1);
+        for(int i=0;i<n;i++){
+            int temp=i;
+            while(temp!=n && s[temp]=='D'){
+                temp++;
+            }
+            reverse(v.begin() + i, v.begin() + temp+1);
+            if(temp!=i) i=temp-1;
+        }
+        for(int i=0;i<=n;i++) ans+=v[i]+'0';
+        return ans;
+    }
+};
