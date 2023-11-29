27027bd01afa701370cc550ec3482ad22148b1b4
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-29 17:53:13 +0530

    Create Solution.cpp

---
+++ 2023/November/29. Euler circuit and Path/Solution.cpp
@@ -0,0 +1,18 @@
+// Euler circuit and Path
+// November 29, 2023
+// C++ Code
+
+class Solution {
+public:
+    int isEulerCircuit(int V, vector<int>adj[]){
+        int evens =0;
+        int odds =0;
+        for(int i =0;i< V;i++){
+            if(adj[i].size() & 1) odds++;
+            else evens++;
+        }
+        if(evens == V) return 2;
+        else if (odds <= 2) return 1;
+        else return 0;
+    }
+};
