88805216277f27c4e2a9dc39b36f82b71ba893e3
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-11-30 14:21:57 +0530

    Create Solution.cpp

---
+++ 2023/November/30. Shortest path from 1 to n/Solution.cpp
@@ -0,0 +1,17 @@
+// Shortest path from 1 to n
+// November 30, 2023
+// C++ Code
+
+class Solution{   
+public:
+    int minimumStep(int n){
+        int ans = 0;
+        
+        while(n >= 3){
+            ans += n % 3 + 1;
+            n /= 3;
+        }
+        
+        return ans + (n - 1);
+    }
+};
