a3a4648a39bd6178ed1c9ba0eaa09ba1b4a85ff5
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-12-05 23:02:45 +0530

    Create Solution.cpp

---
+++ 2023/December/05. Minimize the Heights II/Solution.cpp
@@ -0,0 +1,18 @@
+// Minimize the Heights II
+// December 5, 2023
+// C++ Code
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        sort(arr, arr + n);
+        int res = arr[n-1] - arr[0];
+        int i = lower_bound(arr, arr + n, k) - arr;
+        for(int j = max(i, 1); j < n; j++) {
+            int mini = min(arr[0] + k, arr[j] - k);
+            int maxi = max(arr[n-1] - k, arr[j-1] + k);
+            res = min(res, maxi - mini);
+        }
+        return res;
+    }
+};
