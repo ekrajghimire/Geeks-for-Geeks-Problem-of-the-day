6eaa7412525adda37a5efc34fcec0959566a3988
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-12-18 23:18:41 +0530

    Create Solution.cpp

---
+++ 2023/December/19. Game of XOR/Solution.cpp
@@ -0,0 +1,17 @@
+// Game of XOR
+// December 18, 2023
+// C++ Code
+
+class Solution {
+  public:
+    int gameOfXor(int n, int A[]) {
+        int ans = 0;
+        
+        for(int i=0; i<n; i++){
+            long long ct = (i+1)*(n-i);
+            if(ct%2) ans^=A[i];
+        }
+        
+        return ans;
+    }
+};
