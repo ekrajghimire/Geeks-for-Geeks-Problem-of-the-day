a7da9d2f2c22ae7c70f2523d500c390f242fd1fd
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2024-01-01 16:38:39 +0545

    Create Solution.cpp

---
+++ 2024/01. January/Array Pair Sum Divisibility Problem/Solution.cpp
@@ -0,0 +1,27 @@
+// Array Pair Sum Divisibility Problem
+// January 1, 2024
+// C++ Code
+
+class Solution {
+  public: 
+  bool canPair(vector<int> nums, int k) {
+    auto size = nums.size();
+    if (size % 2 == 1) return false;
+    int arr[100001] = {
+      0
+    };
+    for (auto & val: nums) {
+      int remainder = val % k;
+      int remainderComp = k - remainder;
+      if (arr[remainderComp] > 0) {
+        arr[remainderComp]--;
+      } else {
+        arr[remainder]++;
+      }
+    }
+    for (int i = 1; i <= 100001; i++) {
+      if (arr[i] > 0) return false;
+    }
+    return true;
+  }
+};
